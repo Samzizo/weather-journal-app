a59198a8c08bcc000b155b0257bead26b8973667
Samiha Amroune <68222563+Samzizo@users.noreply.github.com>
2021-07-01 02:47:39 +0300

    first commit

--- server.js
+++ server.js
@@ -1,27 +1,49 @@
+// Setup empty JS object to act as endpoint for all routes
 projectData = {};
+
+// Require Express to run server and routes
 const express = require('express');
+
+// Start up an instance of app
 const app = express();
+
 const bodyParser = require('body-parser');
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
+
+// Initialize the main project folder
 app.use(express.static('website'));
+
+// Setup Server
 const port = 3000;
 const server = app.listen(port, listening);
+
 function listening(){
+    console.log("Hi There I'm running :)");
     console.log(`running on localhost: ${port}`);
-};
+}
+
+//GET Route:
 app.get('/all', sendData);
 function sendData (req, res) {
     res.send(projectData);
-};
+}
+
+//POST Route receiving: temperature – date – user response
 app.post('/add', callback);
 function callback(req, res){
-	projectData = {
-		temperature: req.body.temperature,
-		date: req.body.date,
-		userResponse: req.body.userResponse
-	}
-	res.send(projectData);
+    projectData = {
+        temperature: req.body.temperature,
+        date: req.body.date,
+        userResponse: req.body.userResponse
+    }
+    res.send(projectData);
 }
--- website/app.js
+++ website/app.js
@@ -1,14 +1,15 @@
 /* Global Variables */
+
 const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 const apiKey = '&appid=33821d55ca5d3c735466c594cb39326f&units=metric';
+
 // Create a new date instance dynamically with JS
+
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
-//entryholder divs
-const date = document.getElementById('date');
-const temp = document.getElementById('temp');
-const content = document.getElementById('content');
+
 //ASYNC get from API
+
 const retrieveData = async (baseUrl, zip, apiKey) => {
 	const request = await fetch(baseUrl + zip + apiKey);
 	try {
@@ -21,6 +22,7 @@ const retrieveData = async (baseUrl, zip, apiKey) => {
 }
 
 //ASYNC get from Local
+
 const updateUI = async () => {
     const request = await fetch("/all");
     try {
@@ -32,7 +34,9 @@ const updateUI = async () => {
         console.log("error", error);
     }
 };
+
 //ASYNC post
+
 const postData = async (url ='', data = {}) => {
 	const response = await fetch(url, {
 		method: 'POST',
@@ -49,21 +53,25 @@ const postData = async (url ='', data = {}) => {
 		console.log('error', error);
 	}
 }
+
 //CHAIN: post & get
+
 document.getElementById('generate').addEventListener('click', postRetrieve);
+
 function postRetrieve() {
 	//get user input
 	let zip = document.getElementById('zip').value;
 	let feeling = document.getElementById('feelings').value;
+
 	retrieveData(baseUrl, zip, apiKey) //get data from api
 	.then(function (data) {
-    	postData("/add", {
+		postData ("/add", {
 			//bundle user data and api, and store
 			temperature: data.main.temp,
 			date: newDate,
 			userResponse: feeling,
-    	}).then(function () {
-		updateUI("/data");
-    	});
-	});
+		}).then(function () {
+			updateUI("/data");
+		});
+	})
 }
